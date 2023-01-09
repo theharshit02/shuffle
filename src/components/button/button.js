815745119d124d0195671da93699e867aa427f35
Harshit Mishra <harshitmishra2011@gmail.com>
2023-01-09 21:33:48 +0530

    added category api

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './button.module.css'
+import axios from 'axios'
 
-const button = () => {
+const Button = () => {
+  const [fetchCats, setfetchCats] = useState([]);
+
+  async function getCategory(){
+    const url="http://localhost:3000/api/discover/listCategories"
+    const result= await axios.get(url)
+    if(result.data){
+      // console.log("result",result.data);
+      setfetchCats(result.data);
+    } 
+  }
+
+  useEffect(() => {
+    getCategory()
+  }, []);
+
   return (
     <div>
-      <button className={styles.button}>Category</button>
+
+      {/* <button onClick={getCategory}>click me</button> */}
+      {/* {getCategory()} */}
+      {console.log(fetchCats)}
+      <button className={styles.button}>{fetchCats[0]}</button>
+      <button className={styles.button}>{fetchCats[1]}</button>
+      {/* <button className={styles.button}>hello</button>
+      <button className={styles.button}>hello</button> */}
     </div>
   )
 }
 
-export default button
+export default Button
--- src/components/category/category.js
+++ src/components/category/category.js
@@ -3,13 +3,12 @@ import Button from '../button/button'
 import Filter from '../filter/filter'
 import styles from './category.module.css'
 
+
 const category = () => {
+
   return (
     <div>
-      <div className={styles.category}>   
-        <Button />
-        <Button />
-        <Button />
+      <div className={styles.category}>
         <Button />
       </div>
       <div className={styles.filter}>
